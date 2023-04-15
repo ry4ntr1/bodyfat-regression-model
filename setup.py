91b0b7eb7ee4fa7eba8bbe985cf14d59fcd1e666
Ryan Tri <ry4ntr1@gmail.com>
2023-04-14 21:44:51 -0500

    Github Setup + Sync, Setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    get requirements
+    '''
+    requirements=[]
+    with open(file_path) as fObj:
+        requirements=fObj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='model-deploy',
+    version='0.0.1',
+    author='Ryan Tri',
+    author_email='ry4ntr1@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)

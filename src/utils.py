00f99e46526b43b65ab181eb1b7bf0cb8f5f93cb
Ryan Tri <ry4ntr1@gmail.com>
2023-04-20 14:46:11 -0500

    Model Data Ingestion, Transformation, and Trainer Implemented, Resulting model.pkl stored in artifacts, catboost directory updated

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,9 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
-from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -20,3 +21,31 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+
+    try:
+        
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            # train the model
+            model.fit(x_train, y_train)
+            
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
